e79817a5e1748a39ae1f22c6d3f8633055b008bc
mango0110 <1119369173@qq.com>
2018-10-15 22:17:57 +0800

    v1.0.0

---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+#![feature(fnbox)]
+
+use std::boxed::FnBox;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+struct Worker {
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Drop for Worker {
+    fn drop(&mut self) {
+        if let Some(t) = self.thread.take() {
+            t.join().unwrap();
+        }
+    }
+}
+
+enum Job {
+    NewJob(Box<FnBox() + Send>),
+    Terminate,
+}
+
+pub struct ThreadPool {
+    sender: mpsc::Sender<Job>,
+    workers: Vec<Worker>,
+}
+
+impl ThreadPool {
+    pub fn new(thread_num: usize) -> ThreadPool {
+        let mut workers = Vec::with_capacity(thread_num);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for _ in 0..thread_num {
+            let receiver = receiver.clone();
+            workers.push(Worker {
+                thread: Some(thread::spawn(move || loop {
+                    let msg = receiver.lock().unwrap().recv().unwrap();
+                    match msg {
+                        Job::NewJob(f) => f.call_box(()),
+                        Job::Terminate => break,
+                    }
+                })),
+            })
+        }
+
+        ThreadPool { sender, workers }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        self.sender.send(Job::NewJob(Box::new(f))).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in 0..self.workers.len() {
+            self.sender.send(Job::Terminate).unwrap()
+        }
+        self.workers.clear();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
